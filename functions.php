62ba40eca940b066f600d07a59d55caf89a2d6c2
deathguuun <deathguuun@gmail.com>
2021-10-28 21:36:19 +0300

    last commit

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function assets_version()
 {
-    $assets_version = "5256";
+    $assets_version = "61138";
     return $assets_version;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -26,29 +26,16 @@ $(document).ready(function () {
 
 
 //Acordeon
-$(".question__title").on("click", function () {
-	if ($(this).hasClass("active")) {
-		$(this).removeClass("active");
-		$(this)
-			.siblings(".question__content")
-			.slideUp(200);
-		$(this).find(".question__icon")
-			.removeClass("clicked")
-	} else {
-		$(this).find(".question__icon")
-			.removeClass("clicked")
-		$(this)
-			.find(".question__icon")
-			.addClass("clicked");
-		$(".set > a").removeClass("active");
-		$(this).addClass("active");
-		$(".content").slideUp(200);
-		$(this)
-			.siblings(".question__content")
-			.slideDown(200);
-	}
-});
+$(document).ready(function () {
+	$('.block__title').click(function (event) {
+		if ($('.block').hasClass('one')) {
+			$('.block__title').not($(this)).removeClass('active');
+			$('.block__text').not($(this).next()).slideUp(300);
+		}
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
 
+});
 
 //Adaptive SlickSlider REVIEW
 $(window).on('resize', function (e) {
